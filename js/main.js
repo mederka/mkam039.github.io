bd900e81c4a2dbfb6731dd853e7c89e4bb7428aa
Meder Kamalov <meder@kamalov.net>
2019-01-09 22:05:49 +0100

    Added a rudimentary org parser

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+function readOrg(org){
+  let orgArray = org.split('\n* ');
+  let output = {};
+  for (let i = 1; i < orgArray.length; i++){
+    let item = orgArray[i].split('\n');
+    output[i] = {};
+    output[i].title = item.shift();
+    output[i].content = item.join('\n');
+  }
+  return output;
+}
+
+fetch('https://kamalov.net/files/info.org')
+  .then(response => response.text())
+  .then(response => {
+    let org = response;
+    org = readOrg(org);
+    console.log(org);
+  });
