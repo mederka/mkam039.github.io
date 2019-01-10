aacc4f255e7521fc3935b66000aa74343cb4b5c5
Meder Kamalov <meder@kamalov.net>
2019-01-10 22:34:53 +0100

    Underscored element selection and clicking

--- js/main.js
+++ js/main.js
@@ -10,10 +10,23 @@ function readOrg(org){
   return output;
 }
 
+function underscoreClick(arg){
+  return () => console.log(arg);
+}
+
+function addUnderscoreListeners(){
+  var all = document.getElementsByTagName('*');
+  for (let i = 0; i < all.length; i++){
+    if (all[i].id && all[i].id[0] == '_'){
+      all[i].addEventListener('click', underscoreClick(all[i].id));
+    }
+  }
+}
+
 fetch('http://kamalov.net/files/info.org')
   .then(response => response.text())
   .then(response => {
     let org = response;
     org = readOrg(org);
-    console.log(org);
+    addUnderscoreListeners();
   });
