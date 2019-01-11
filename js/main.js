fba73842b15619de2293cc7f798a36029452b59a
Meder Kamalov <meder@kamalov.net>
2019-01-11 15:20:27 +0100

    Pass org into the underscoreClick function

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
-function readOrg(org){
-  let orgArray = org.split('\n* ');
+function readOrg(org, level=1){
+  let orgArray = org.split('\n' + '*'.repeat(level) + ' ');
   let output = {};
   for (let i = 1; i < orgArray.length; i++){
     let item = orgArray[i].split('\n');
-    output[i] = {};
-    output[i].title = item.shift();
-    output[i].content = item.join('\n');
+    const id = item.shift();
+    output[id] = item.join('\n');
   }
   return output;
 }
 
-function underscoreClick(arg){
-  return () => console.log(arg);
+function underscoreClick(id, org){
+  return () => console.log(org[id]);
 }
 
-function addUnderscoreListeners(){
+function addUnderscoreListeners(org){
   var all = document.getElementsByTagName('*');
   for (let i = 0; i < all.length; i++){
     if (all[i].id && all[i].id[0] == '_'){
-      all[i].addEventListener('click', underscoreClick(all[i].id));
+      all[i].addEventListener('click', underscoreClick(all[i].id, org));
     }
   }
 }
 
-fetch('http://kamalov.net/files/info.org')
+fetch('files/info.org')
   .then(response => response.text())
   .then(response => {
     let org = response;
     org = readOrg(org);
-    addUnderscoreListeners();
+    addUnderscoreListeners(org);
   });
