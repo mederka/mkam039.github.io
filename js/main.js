0c16da1b7ba7b0ffd7c64dff5e2f3893cf4761a7
Meder Kamalov <meder@kamalov.net>
2019-01-21 21:04:18 +0100

    Removed animated buttons from index body

--- js/main.js
+++ js/main.js
@@ -43,9 +43,6 @@ function addInternalLinks() {
   for (let i = 0; i < all.length; i++){
     if (all[i].id && all[i].id[0] == '_'){
       all[i].addEventListener('click', makeLink(all[i].id));
-      setTimeout(() => all[i].classList.add('animate-button'), 1000);
-      setTimeout(() => all[i].classList.add('animate-background'), 1500);
-      setTimeout(() => all[i].classList.add('change-background'), 2500);
     }
   }
 }
